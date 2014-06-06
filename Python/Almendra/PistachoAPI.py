e4f563ed4247bb16275476b4dc97156f6bfd35b0
DarkDruiD <franciscojpiedrahita@gmail.com>
2014-06-06 10:12:10 -0500

    Changed hum to 75

--- Python/Almendra/PistachoAPI.py
+++ Python/Almendra/PistachoAPI.py
@@ -8,14 +8,14 @@ class PistachoAPI(object):
     def __init__(self, local = False):
 	if local:
 	    self.host = 'http://contribhub.com:3000/'
-	else:        
-            self.host = 'http://plantigoshi.herokuapp.com/' 
+	else:
+            self.host = 'http://plantigoshi.herokuapp.com/'
 
     def postData(self, data, plant):
 	#print data
-	data['Status'] = ((data['Hum'] < 70)  and (data ['Hum'] > 50))
+	data['Status'] = ((data['Hum'] < 75)  and (data ['Hum'] > 50))
 	print data
-        url = self.host + 'plants/' + str(plant) + '/sync' 
+        url = self.host + 'plants/' + str(plant) + '/sync'
         request = urllib2.Request(url)
         request.add_header('Content-Type', 'application/json')
         result = urllib2.urlopen(request, json.dumps({'data' : data}))
@@ -24,7 +24,7 @@ class PistachoAPI(object):
 
     def wateringFlag(self, plant):
         url = self.host + 'plants/' + str(plant) + '/sync_water'
-        return urllib2.urlopen(url).read() 
+        return urllib2.urlopen(url).read()
 
     def temperatureFlag(self, plant):
         url = self.host + 'plants/' + str(plant) + '/sync_heat'
